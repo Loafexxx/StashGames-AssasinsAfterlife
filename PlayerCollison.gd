9406a75cbde743e8381ff3aeb7aefc02aadcb33b
Loafexxx <Loafexxx@gmail.com>
2025-09-23 20:18:38 +1000

    Quick Save

--- PlayerCollison.gd
+++ PlayerCollison.gd
@@ -1,1 +1,7 @@
 extends Area2D
+
+func _ready() -> void:
+	connect("area_entered", Callable(self, "_on_Hitbox_area_entered"))
+
+func _on_Hitbox_area_entered(area: Area2D) -> void:
+	print("JUST LET ME SYD")
--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -1,18 +1,22 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-@onready var characterbody2d = $"."
-
-var falling = false
+var accelerate_y = 0.0
 
 func _physics_process(delta: float) -> void:
-	print(linear_velocity.x)
-	if Input.is_action_pressed("Right"):
-		if linear_velocity.x < get_meta("MaxSpeed"):
-			linear_velocity.x += get_meta("PlayerMovementSpeed")
+	accelerate_y += get_meta("Weight") + get_gravity().y
+	if Input.is_action_pressed("Right"): #check if right is pressed
+		if velocity.x < get_meta("MaxSpeed"):
+			velocity.x += get_meta("PlayerMovementSpeed")
 			
-	if Input.is_action_pressed("Left"):
-		if linear_velocity.x > (get_meta("MaxSpeed") * -1):
-			linear_velocity.x -= (get_meta("PlayerMovementSpeed"))
-	if Input.is_action_just_released("Up"):
-		linear_velocity.y -= (get_meta("JumpHeight")*50)
-	angular_velocity = 0
+	if Input.is_action_pressed("Left"): #check if Left is pressed
+		if velocity.x > (get_meta("MaxSpeed") * -1):
+			velocity.x -= (get_meta("PlayerMovementSpeed"))
+			
+	if Input.is_action_just_pressed("Up"): #check if Up is pressed
+		velocity.y = (get_meta("JumpHeight")*50*-1)
+	move_and_slide()
+	
+	velocity.y = accelerate_y
+	
+func _on_Area2D_area_entered(area):
+	print("dih")
