ebc1bc635e5d6d0d20449375131b9f319b3a7e6d
Loafexxx <Loafexxx@gmail.com>
2025-09-24 21:34:26 +1000

    Added a simple boss move toward player script
    I added A EnemyMoveTowardPlayer.gd script to detect if player is in range and move to them, now i will add the arena.

--- EmemyMoveToPlayer.gd
+++ EmemyMoveToPlayer.gd
@@ -1,46 +1,38 @@
 extends Area2D
 
-@onready var player = $"../../../../Player"
-@onready var playerChar = $"../../../../Player/CharacterBody2D"
-@onready var self_rigidbody = $".."
+@onready var playerChar = $"../../../../Player/CharacterBody2D" #Makes a variable for the players CharacterBody2D
+@onready var self_rigidbody = $".." #Makes a variable for the bosses Rigidbody2D
 
-var canmove = false
-var left = false
+var detecting = false #detecting is checking it should detect what side the player is on and moves to them
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	connect("area_entered", _on_area_entered)
-	connect("area_exited", _on_area_exited)
+	connect("area_entered", _on_area_entered) #just connects the area enterd function to the _on_area_entered func
+	connect("area_exited", _on_area_exited) #just connects the area exited function to the _on_area_exited func
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if canmove:
-		if left:
-			self_rigidbody.linear_velocity.x = get_meta("X_Speed")
+
+	if detecting:
+		if get_meta("MoveX"): #sees the axis it needs to move to (in this case X)
+			if playerChar.global_position.x < self_rigidbody.global_position.x: #checks to see if the player is on the left, if they are the boss moves towards them
+				self_rigidbody.linear_velocity.x = (get_meta("X_Speed")*-1) #sets the bosses X velocity
+				
+			if playerChar.global_position.x > self_rigidbody.global_position.x: #checks to see if the player is on the right, if they are the boss moves towards them
+				self_rigidbody.linear_velocity.x = get_meta("X_Speed") #sets the bosses X velocity
+				
+		if get_meta("MoveY"): #sees the axis it needs to move to (in this case Y)
+			if playerChar.global_position.y < self_rigidbody.global_position.y: #checks to see if the player is beneath, if they are the boss moves towards them
+				self_rigidbody.linear_velocity.y = (get_meta("Y_Speed")*-1) #sets the bosses Y velocity
+				
+			if playerChar.global_position.y > self_rigidbody.global_position.y: #checks to see if the player is above, if they are the boss moves towards them
+				self_rigidbody.linear_velocity.y = get_meta("Y_Speed") #sets the bosses Y velocity
+
 			
 	
-func _on_area_entered(area: Area2D) -> void:
-	if get_meta("MoveX"):
-		if playerChar.position.x > position.x:
-			print("Right")
-			canmove = true
-			left = false
-		if playerChar.position.x < position.x:
-			print("Left")
-			canmove = true
-			left = true
+func _on_area_entered(area: Area2D) -> void: #when the player is in range of the boss it turns on detecting which locates what side to move to
+	detecting = true 
 
-	if get_meta("MoveY"):
-		if playerChar.position.y > position.y:
-			print("Down")
-			canmove = true
-			left = false
-		if playerChar.position.y < position.y:
-			print("Up")
-			canmove = true
-			left = true
-
-func _on_area_exited(area: Area2D) -> void:
-	canmove = false
-	print("SMD")
+func _on_area_exited(area: Area2D) -> void: #turns off detecting
+	detecting = false
--- EnemyAttraction.gd
+++ EnemyAttraction.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var area2d = $Area2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
